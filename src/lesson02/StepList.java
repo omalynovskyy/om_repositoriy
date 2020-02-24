b532fbf7814be776698d10f2a33d3eba47c648d3
omalynovskyy <oleksiy.malynovskyy@gmail.com>
2020-02-24 21:30:45 +0200

    lesson02 eachNth corrected

--- src/lesson02/StepList.java
+++ src/lesson02/StepList.java
@@ -2,27 +2,20 @@ package lesson02;
 
 
 public class StepList {
-    static String  str;
-    static int step;
 
-    public StepList(String str, int step) {
-        this.str = str;
-        this.step = step;
-    }
-    public void eachNth(){
-        int i=step;
+    public static String  eachNth(String  str, int step){
         int len = str.length();
         String test = String.valueOf(str.charAt(0));
-        while (i < len)
+        while (step < len)
         {
-            test = test.concat(String.valueOf(str.charAt(i)));
-            i+=step;
+            test = test.concat(String.valueOf(str.charAt(step)));
+            step+=step;
         }
-        System.out.println(test);
+        return test;
     }
     public static void main(String[] args) {
-        StepList result = new StepList("avbfckdoeof",2);
-        result.eachNth();
+        System.out.println(eachNth("anbrcjdpe",2));
+        System.out.println(eachNth("anfbgrctjdoper",3));
     }
 
 }
